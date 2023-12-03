5d8478ae1151d306a4c04c2a75cd5b293ce74efb
Max Merger <kyle.iswitcherus@gmail.com>
2023-12-03 21:59:46 +0100

    add logger

---
+++ log.py
@@ -0,0 +1,24 @@
+import logging
+
+class Log:
+    def __init__(self):
+        self.logger = logging.getLogger(__name__)
+        self.logger.setLevel(logging.INFO)
+        
+        handler = logging.StreamHandler()
+        handler.setLevel(logging.INFO)
+        formatter = logging.Formatter('%(asctime)s | %(levelname)s | %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+    
+    
+    def info(self, text):
+        self.logger.info(text)
+    
+        
+    def warning(self, text):
+        self.logger.warning(text)
+    
+        
+    def critical(self, text):
+        self.logger.critical(text)
