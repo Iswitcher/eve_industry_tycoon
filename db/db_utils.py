2c0f8dd77c42386f8da1e743afe097b7fce68840
Max Merger <kyle.iswitcherus@gmail.com>
2023-12-07 21:02:06 +0100

    add table columns logic

--- db/db_utils.py
+++ db/db_utils.py
@@ -64,4 +64,59 @@ class db_utils:
         
     # create table
     def table_create(self, table: str):
-        blah = 123
+        try:
+            _cursor = self.db_conn.cursor()
+            q = (f"""
+                CREATE TABLE {table}
+                (
+                    checksum    TEXT,
+                    start_date  DATE,
+                    end_date    DATE
+                )""")
+            _cursor.execute(q)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            lg.critical(f'ERROR in {method_name}: {e}')
+        
+    
+    # check if table columns exist
+    def table_column_check(self, table: str, columns, types):
+        try:
+            if len(columns) != len(types):
+                raise Exception("Mismatch lenght of columns/types!")
+            _cursor = self.db_conn.cursor()
+            _cursor.execute(f"PRAGMA table_info({table})")
+            rows = _cursor.fetchall()
+            column_names = [row[1] for row in rows]
+            for i, att in enumerate(columns):
+                if self.cnt_col_occur(att, column_names) > 0:
+                    continue
+                type = types[i]
+                self.add_table_column(table, att, type)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            lg.critical(f'ERROR in {method_name}: {e}')
+            
+    
+    # counts the occurence of column name in array of names
+    def cnt_col_occur(self, value, array):
+        cnt = 0
+        for i in array:
+            if i == value:
+                cnt = cnt + 1
+        return cnt
+    
+    
+    # adds new table column of specified type
+    def add_table_column(self, table, att_name, att_type):
+        try:
+            _cursor = self.db_conn.cursor()
+            alter_query = f"""
+                ALTER TABLE {table} 
+                ADD COLUMN {att_name} {att_type}
+                """
+            _cursor.execute(alter_query)
+            self.db_conn.commit()
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            lg.critical(f'ERROR in {method_name}: {e}')
--- db/sde2db.py
+++ db/sde2db.py
@@ -2,6 +2,10 @@ import yaml
 
 from db.db_utils import db_utils
 
+import traceback
+from log import log
+lg = log(None)
+
 class sde2db:
     
     def __init__(self, checksum):
@@ -14,16 +18,45 @@ class sde2db:
     # main conversion method
     def sde_convert_all(self):
         self.sde_covert_categoryids()
+        # TODO other types
         
-        
-    # convert categoryIDs.yaml to DB
-    def sde_covert_categoryids(self):    
-        with open(self.sde_categories, 'r', encoding="utf-8") as yaml_file:
-            yaml_data = yaml.safe_load(yaml_file)
+    
+    # read yaml file
+    def sde_yaml_read(self, path):
+        try:
+            with open(path, 'r', encoding="utf-8") as yaml_file:
+                yaml_data = yaml.safe_load(yaml_file)
+                return yaml_data
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            lg.critical(f'ERROR in {method_name}: {e}')
             
+            
+    # check if tables exists and check columns
+    def sde_table_check(self, table, columns, types):
         db = db_utils(self.sde_db_path, None)
         conn = db.db_connect()
-        blah = 123
-        # TODO import logic
+        
+        if not db.table_check(table):
+            db.table_create(table)
+        db.table_column_check(table, columns, types)
+    
+    
+    # convert categoryIDs.yaml to DB
+    def sde_covert_categoryids(self):    
+        try:
+            table = 'category_ids'
+            columns = ['category_id', 'en']
+            types = ['NUMBER', 'TEXT']
+            
+            self.sde_table_check(table, columns, types)
+            
+            yaml_data = self.sde_yaml_read(self.sde_categories)
+            for row in yaml_data:
+                b = row
+                blah = 123
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')
             
     
