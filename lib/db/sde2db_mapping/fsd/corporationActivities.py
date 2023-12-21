5937ca29e26e9ea12d75061af263f7b90d8d7d82
Max Merger <kyle.iswitcherus@gmail.com>
2023-12-21 14:23:59 +0100

    add corporationActivities

---
+++ lib/db/sde2db_mapping/fsd/corporationActivities.py
@@ -0,0 +1,97 @@
+import traceback
+
+from lib.db.sde2db_mapping.sde_mapper import mapper
+from lib.db.db_utils import db_utils
+from lib.logger import logger
+
+class corporationActivities(mapper):
+    
+    def __init__(self, db: db_utils, log:logger):
+        self.db = db
+        self.log = log
+        self.activities = table_activities()
+        
+    
+    # check if all tables are present
+    def check(self):
+        try:
+            # corporationActivities
+            self.check_table(self.activities)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')
+            
+    
+    # check provided table obj
+    def check_table(self, table_obj):
+        try:
+            if not self.db.table_check(table_obj.table_name):
+                    self.db.table_create(table_obj.table_name)
+            cols = [] 
+            types = []
+            for column in table_obj.columns:
+                    cols.append(column.name)
+                    types.append(column.type)
+            self.db.table_column_check(table_obj.table_name, cols, types)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')
+    
+    
+    # start the import
+    def start(self):
+        try:
+            self.db.table_start_sync(self.activities.table_name)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')
+    
+
+    def run(self, id, row):
+        try:
+            self.add_activity(self.activities.table_name, self.activities.table_pk, id, row)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')
+    
+    
+    # complete the import
+    def finish(self):
+        try:
+            self.db.table_finish_sync(self.activities.table_name)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')           
+         
+    
+    # add or update a attribute        
+    def add_activity(self, table, pk, id, row):
+        try:
+            columns = []
+            values = []
+            
+            for column in self.activities.columns:
+                value = self.yaml_value_extract(id, row, column.path)
+                columns.append(column.name)
+                values.append(value)
+            self.db.record_add_or_replace(table, pk, id, columns, values)
+        except Exception as e:
+            method_name = traceback.extract_stack(None, 2)[0][2]
+            self.log.critical(f'ERROR in {method_name}: {e}')
+        
+
+class col:
+    def __init__(self, name, type = 'TEXT', path = None):
+        self.name = name
+        self.type = type
+        self.path = path
+    
+
+class table_activities:
+    def __init__(self):
+        self.table_name = 'corporation_activities'
+        self.table_pk = 'activity_id'
+        self.columns = []
+        
+        self.columns.append(col('activity_id', 'NUMBER', '#root'))
+        self.columns.append(col('name_en',     'TEXT', 'nameID/en'))
