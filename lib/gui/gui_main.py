7a4179b2e14ca3fad1350b4b57344847853c1397
Max Merger <kyle.iswitcherus@gmail.com>
2024-01-05 21:33:18 +0100

    update gui main

--- lib/gui/gui_main.py
+++ lib/gui/gui_main.py
@@ -2,13 +2,12 @@
 import tkinter as tk
 from tkinter import messagebox
 from tkinter import ttk
-from PIL import Image, ImageTk
 
 from logic import logic
 from lib.logger import logger
 
 from lib.gui.main.menubar import menubar
-from lib.gui.main.navbar import navbar
+# from lib.gui.main.navbar import navbar
 
 class gui_main:
 
@@ -21,6 +20,15 @@ class gui_main:
         self.height = 768
         
         self.window = tk.Tk()
+        
+        self.menu = menubar(self.log, self.logic, self.window)
+        self.utils = tk.Frame()
+        
+        from lib.gui.main.navbar import navbar
+        self.navbar = navbar(self.log, self.window)
+        
+        self.main = tk.Frame()
+        self.status = tk.Frame()
 
 
     def run_window(self):
@@ -28,87 +36,84 @@ class gui_main:
         self.window.minsize(self.width, self.height)
         self.window.geometry(f"{self.width}x{self.height}")
         
-        menu = menubar(self.log, self.logic, self.window)
-        menu.create_menubar()
+        self.set_window_layout()
         
-        self.set_layout(self.window)
         self.window.mainloop()
 
 
-    def set_layout(self, window):
+    def set_window_layout(self):
+        # add dropdown menubar
+        self.menu.create_menubar()
+        
         # add bottom area for status text and progressbars
-        status_frame = self.init_frame_status(window)
-        self.populate_frame_status(status_frame)
+        self.init_frame_status()
+        self.populate_frame_status()
 
         # universal tools?
-        utils_frame = self.init_frame_utils(window)
-        self.populate_frame_utils(utils_frame)
+        self.init_frame_utils()
+        self.populate_frame_utils()
         
         # navigation frame
-        nav_frame = navbar(self.log, self.logic, self.window)
-        nav_frame.navbar_init()
+        self.navbar.navbar_init()
         
         # main area, content depends on selected nav menu
-        main = self.init_frame_main(window)
-        self.populate_frame_main(main)
+        self.init_frame_main()
+        self.populate_frame_main()
 
 
-    def init_frame_status(self, parent):
-        frame = tk.Frame(master=parent)
-        frame.config(height=10)
-        frame.config(highlightbackground="gray")
-        frame.config(highlightthickness=1)
+    def init_frame_status(self):
+        self.status.master = self.window
+        self.status.config(height=10)
+        self.status.config(highlightbackground="gray")
+        self.status.config(highlightthickness=1)
         
-        frame.pack_configure(fill=tk.BOTH)
-        frame.pack_configure(side=tk.BOTTOM)
-        frame.pack_configure(expand=False)
-        frame.pack
-        return frame
+        self.status.pack_configure(fill=tk.BOTH)
+        self.status.pack_configure(side=tk.BOTTOM)
+        self.status.pack_configure(expand=False)
+        self.status.pack
 
 
-    def init_frame_utils(self, parent):
-        frame = tk.Frame(master=parent)
-        frame.config(height=50)
-        frame.config(bg="light gray")
-        frame.config(highlightbackground="gray")
-        frame.config(highlightthickness=1)
+    def init_frame_utils(self):
+        self.utils.master = self.window
+        self.utils.config(height=50)
+        self.utils.config(bg="light gray")
+        self.utils.config(highlightbackground="gray")
+        self.utils.config(highlightthickness=1)
         
-        frame.pack_configure(fill=tk.BOTH)
-        frame.pack_configure(side=tk.TOP)
-        frame.pack_configure(expand=False)
-        frame.pack
-        return frame
+        self.utils.pack_configure(fill=tk.BOTH)
+        self.utils.pack_configure(side=tk.TOP)
+        self.utils.pack_configure(expand=False)
+        self.utils.pack
 
 
-    def init_frame_main(self, parent):
-        frame = tk.Frame(master=parent)
-        frame.config(height=600)
-        frame.config(width=800)
-        frame.config(highlightbackground="gray")
-        frame.config(highlightthickness=1)
+    def init_frame_main(self):
+        self.main.master = self.window
+        self.main.config(height=600)
+        self.main.config(width=800)
+        self.main.config(highlightbackground="gray")
+        self.main.config(highlightthickness=1)
         
-        frame.pack_configure(fill=tk.BOTH)
-        frame.pack_configure(side=tk.LEFT)
-        frame.pack_configure(expand=True)
-        return frame
+        self.main.pack_configure(fill=tk.BOTH)
+        self.main.pack_configure(side=tk.LEFT)
+        self.main.pack_configure(expand=True)
 
 
-    def populate_frame_status(self, frame):
+    def populate_frame_status(self):
         # add progressbar
-        self.progressbar = ttk.Progressbar(orient="horizontal", master=frame)
+        self.progressbar = ttk.Progressbar(orient="horizontal", master=self.status)
         self.progressbar.pack(fill='x', side="right", padx=6, pady=3)
         
-        utils_label = tk.Label(master=frame, text="Status panel")
+        utils_label = tk.Label(master=self.status, text="Status panel")
         utils_label.place(x=0, y=0) 
 
 
-    def populate_frame_utils(self, frame):
-        utils_label = tk.Label(master=frame, text="App tools panel")
+    def populate_frame_utils(self):
+        utils_label = tk.Label(master=self.utils, text="App tools panel")
         utils_label.place(x=0, y=0) 
 
 
-    def populate_frame_main(self, frame):        
-        nav_label = tk.Label(master=frame, text="Main panel")
+    def populate_frame_main(self):        
+        nav_label = tk.Label(master=self.main, text="Main panel")
         nav_label.pack(side="top")
 
 
@@ -116,3 +121,8 @@ class gui_main:
         result = self.logic.esi_test()
         txtbox.delete(1.0, tk.END)
         txtbox.insert(tk.END, result)
+
+
+    def frame_main_switch_mode(self, mode:str):
+        # TODO switch main frame based on imput
+        blah = 123
