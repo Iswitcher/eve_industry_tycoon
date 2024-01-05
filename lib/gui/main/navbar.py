cf24fec708ee10c95dc5849413831a0cfb2f199c
Max Merger <kyle.iswitcherus@gmail.com>
2024-01-05 20:32:20 +0100

    add navbar logic

--- lib/gui/main/navbar.py
+++ lib/gui/main/navbar.py
@@ -2,14 +2,13 @@ import tkinter as tk
 from PIL import Image, ImageTk
 
 from lib.logger import logger
-from logic import logic
+from lib.gui.gui_main import gui_main
 
-class navbar:
+class navbar(gui_main):
     """A navigation bar element, used in main UI window
     """
-    def __init__(self, log: logger, logic: logic, window: tk):
+    def __init__(self, log: logger, window: tk):
         self.log = log
-        self.logic = logic
         self.window = window
         self.frame = tk.Frame()
 
@@ -19,6 +18,7 @@ class navbar:
         self.frame_configure()
         self.frame_add_buttons()
         self.frame_pack()
+        self.on_startup()
         return self.frame
 
 
@@ -36,12 +36,15 @@ class navbar:
         self.frame.pack
 
 
-    def on_button_pressed(self, btn: tk.Button):
-        blah = 123
+    # whenever a button is pressed - forward selected mode to parent class
+    def on_button_pressed(self, mode: str):
+        super().frame_main_switch_mode(mode)
 
 
+    # initial mode on app start
     def on_startup(self):
-        blah = 123
+        mode = 'market'
+        super().frame_main_switch_mode(mode)
 
 
     def frame_add_buttons(self):
@@ -63,12 +66,12 @@ class navbar:
         btn.config(compound='top')
         btn.config(width=46)
         btn.config(height=64)
-        btn.config(command=lambda: self.on_button_pressed(btn))
+        btn.config(command=lambda: self.on_button_pressed('market'))
         btn.pack(fill='x', side=side)
 
 
     def add_button_contracts(self, side: str):
-        path = 'assets/gui/nav_panel/Market.png'
+        path = 'assets/gui/nav_panel/Contracts.png'
         global img_nav_contracts
         img_nav_contracts = ImageTk.PhotoImage(Image.open(path).resize((42,42)))
         btn = tk.Button(self.frame, width=46, height=64)
@@ -77,12 +80,12 @@ class navbar:
         btn.config(compound='top')
         btn.config(width=46)
         btn.config(height=64)
-        btn.config(command=lambda: self.on_button_pressed(btn))
+        btn.config(command=lambda: self.on_button_pressed('contracts'))
         btn.pack(fill='x', side=side)
 
 
     def add_button_industry(self, side: str):
-        path = 'assets/gui/nav_panel/Market.png'
+        path = 'assets/gui/nav_panel/Industry.png'
         global img_nav_industry
         img_nav_industry = ImageTk.PhotoImage(Image.open(path).resize((42,42)))
         btn = tk.Button(self.frame, width=46, height=64)
@@ -91,12 +94,12 @@ class navbar:
         btn.config(compound='top')
         btn.config(width=46)
         btn.config(height=64)
-        btn.config(command=lambda: self.on_button_pressed(btn))
+        btn.config(command=lambda: self.on_button_pressed('industry'))
         btn.pack(fill='x', side=side)
 
 
     def add_button_assets(self, side: str):
-        path = 'assets/gui/nav_panel/Market.png'
+        path = 'assets/gui/nav_panel/Assets.png'
         global img_nav_assets
         img_nav_assets = ImageTk.PhotoImage(Image.open(path).resize((42,42)))
         btn = tk.Button(self.frame, width=46, height=64)
@@ -105,12 +108,12 @@ class navbar:
         btn.config(compound='top')
         btn.config(width=46)
         btn.config(height=64)
-        btn.config(command=lambda: self.on_button_pressed(btn))
+        btn.config(command=lambda: self.on_button_pressed('assets'))
         btn.pack(fill='x', side=side)
 
 
     def add_button_map(self, side: str):
-        path = 'assets/gui/nav_panel/Market.png'
+        path = 'assets/gui/nav_panel/Map.png'
         global img_nav_map
         img_nav_map = ImageTk.PhotoImage(Image.open(path).resize((42,42)))
         btn = tk.Button(self.frame, width=46, height=64)
@@ -119,12 +122,12 @@ class navbar:
         btn.config(compound='top')
         btn.config(width=46)
         btn.config(height=64)
-        btn.config(command=lambda: self.on_button_pressed(btn))
+        btn.config(command=lambda: self.on_button_pressed('map'))
         btn.pack(fill='x', side=side)
 
 
     def add_button_debug(self, side: str):
-        path = 'assets/gui/nav_panel/Market.png'
+        path = 'assets/gui/nav_panel/Debug.png'
         global img_nav_debug
         img_nav_debug = ImageTk.PhotoImage(Image.open(path).resize((42,42)))
         btn = tk.Button(self.frame, width=46, height=64)
@@ -133,5 +136,5 @@ class navbar:
         btn.config(compound='top')
         btn.config(width=46)
         btn.config(height=64)
-        btn.config(command=lambda: self.on_button_pressed(btn))
+        btn.config(command=lambda: self.on_button_pressed('debug'))
         btn.pack(fill='x', side=side)
